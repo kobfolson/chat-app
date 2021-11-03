ba1e8399758ed1da81339b51c500df95e92bcdac
kobfolson <kobbyfolson@gmail.com>
2021-11-03 16:32:43 +0000

    feat: add regex to find svg imports and transform

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,24 @@
 module.exports = {
   moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)(\\?(inline|raw))?$':
+      'jest-transform-stub',
+    // '.+\\.svg?.+$': 'jest-transform-stub',
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    '^vue$': 'vue/dist/vue.common.js',
   },
-  moduleFileExtensions: [
-    'js',
-    'vue',
-    'json'
-  ],
+  moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    // '.+\\.svg?.+$': 'jest-transform-stub',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)(\\?(inline|raw))?$':
+      'jest-transform-stub',
   },
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/pages/**/*.vue',
   ],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
 }
